008037fa65b116c8ef5d43ba4982b067705e31ca
Padee Vang <vangpadee11@gmail.com>
2025-04-30 19:26:37 -0500

    added reducer and saga files for book

---
+++ src/redux/reducers/book.reducer.js
@@ -0,0 +1,11 @@
+
+const books = (state = [], action) => {
+    switch (action.type) {
+      case 'SET_BOOKS':
+        return action.payload;
+      default:
+        return state;
+    }
+  }
+
+  export default books;
---
+++ src/redux/sagas/book.saga.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function fetchBooks(action) {
+
+}
+
+function booksSaga() {
+
+}
+
+export default booksSaga;
